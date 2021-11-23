50052ce7778fc136ec560ea8ab6a499ea03bd57e
Woahisme <rspiewak@mit.edu>
2021-11-23 11:50:46 -0500

    TA help commit

--- index.js
+++ index.js
@@ -16,22 +16,45 @@
 
 
 var express = require('express');
-var app     = express();
-
-app.get('/', function(req,res){
-   console.log(':)), Server New Hit: ' + timeStamp())
-   res.send(':)), Server New Hit yall: ' + timeStamp());
+var app = express();
+var sqldatabase= require('mysql2');
+var connection = sqldatabase.createConnection({
+  host: 'db-mysql-nyc3-finalproject-do-user-9878515-0.b.db.ondigitalocean.com',
+  port: 25060,
+  user: 'doadmin',
+  password: 'fvzhYipQmeNYDLTz',
+  database: 'defaultdb'
 });
 
-function timeStamp(){
-   var currentdate = new Date();
-   var prettyTime = currentdate.getHours() + ":" +
-               currentdate.getMinutes() + ":" +
-               currentdate.getSeconds();
-   return prettyTime;               
-}
+// used to serve static files from public directory
+app.use(express.static('public'));
+
+app.get('/add', function(req, res){
+  console.log('firstName: ', req.query.firstName);
+  console.log('email: ', req.query.email);
+  var responsehtml = "";
+  responsehtml += `<p class="display-1"> hello ${req.query.firstName} </p>`;
+  responsehtml += `<p> Your email is: ${req.query.email} </p>`; 
+  res.send(responsehtml);
+})
 
 var port = 3000;
 app.listen(port,function(){
-   console.log('Listening on port:' + port);
-});
+console.log('Listening on port:' + port);
+
+// app.get('/', function(req,res){
+//    console.log(':)), Server New Hit: ' + timeStamp())
+//    res.send(':)), Server New Hit yall: ' + timeStamp());
+// });
+
+// function timeStamp(){
+//    var currentdate = new Date();
+//    var prettyTime = currentdate.getHours() + ":" +
+//                currentdate.getMinutes() + ":" +
+//                currentdate.getSeconds();
+//    return prettyTime;               
+// };
+
+
+// });
+})
